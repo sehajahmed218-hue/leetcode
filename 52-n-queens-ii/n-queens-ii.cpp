bf9b9ee61abe09cf3851c07753a136d09eafd62c
sehaj ahmed <sehajahmed218@gmail.com>
2026-09-05 00:51:31 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (89.65%) - LeetSync

--- 52-n-queens-ii/n-queens-ii.cpp
+++ 52-n-queens-ii/n-queens-ii.cpp
@@ -1,9 +1,13 @@
 class Solution {
+    int ans=0;
 public:
-    int find(int row,int n,vector<bool>& ldiagonal,vector<bool>& rdiagonal,vector<bool>& column)
+    void find(int row,int n,vector<bool>& ldiagonal,vector<bool>& rdiagonal,vector<bool>& column)
     {
-        if(row==n)  return 1;
-        int ans=0;
+        if(row==n){
+            ans++;
+          return;
+        }
+        
         for(int i=0;i<n;i++)
         {
             if(ldiagonal[n-1+i-row]==0 && rdiagonal[i+row]==0 && column[i]==0)
@@ -11,19 +15,20 @@ public:
                 ldiagonal[n-1+i-row]=1;
                 rdiagonal[i+row]=1;
                 column[i]=1;
-                ans+=find(row+1,n,ldiagonal,rdiagonal,column);
+                find(row+1,n,ldiagonal,rdiagonal,column);
                 ldiagonal[n-1+i-row]=0;
                 rdiagonal[i+row]=0;
                 column[i]=0;
             }
             
         }
-        return ans;
+        return;
     }
     int totalNQueens(int n) {
         vector<bool> ldiagonal(2*n-1,0);
         vector<bool> rdiagonal(2*n-1,0);
         vector<bool> column(n,0);
-        return find(0,n,ldiagonal,rdiagonal,column);
+        find(0,n,ldiagonal,rdiagonal,column);
+        return ans;
     }
 };
