fc49d3866e076a58024af8b106ad313341318a09
sehaj ahmed <sehajahmed218@gmail.com>
2026-09-05 19:53:18 +0530

    Time: 3 ms (61.24%) | Memory: 10.2 MB (77.58%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,38 +1,39 @@
 class Solution {
 public:
-    void find(int row,int n,vector<string>& board,vector<vector<string>>& ans,vector<bool>& column,vector<int>& ldiagonal,vector<int>& rdiagonal){
+    void find(vector<vector<string>>& ans,vector<string>& board,int n,vector<bool>& col,vector<bool>& ld,vector<bool>& rd,int row)
+    {
         if(row==n)
-        {
+        {   
             ans.push_back(board);
             return;
         }
         for(int i=0;i<n;i++)
         {
-         if(ldiagonal[n-1+i-row]==0 && column[i]==0 && rdiagonal[i+row]==0)
-         {
-            column[i]=1;
-            ldiagonal[n-1+i-row]++;
-            rdiagonal[i+row]++;
-            board[row][i]='Q';
-            find(row+1,n,board,ans,column,ldiagonal,rdiagonal);
-            ldiagonal[n-1+i-row]--;
-            rdiagonal[row+i]--;
-            board[row][i]='.';
-            column[i]=0;
-         }   
+            if(!ld[n-1+i-row] && !rd[row+i] && !col[i])
+            {
+                board[row][i]='Q';
+                ld[n-1+i-row]=1;
+                rd[row+i]=1;
+                col[i]=1;
+                find(ans,board,n,col,ld,rd,row+1);
+                board[row][i]='.';
+                ld[n-1+i-row]=0;
+                rd[row+i]=0;
+                col[i]=0;
+            }
         }
     }
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>> ans;
-        vector<string> board(n);
+        vector<string>board(n);
         for(int i=0;i<n;i++)
         for(int j=0;j<n;j++)
         board[i].push_back('.');
         vector<bool> column(n,0);
-        vector<int> ldiagonal(2*n-1,0);
-        vector<int> rdiagonal(2*n-1,0);
+        vector<bool> ld(2*n-1,0);
+        vector<bool> rd(2*n-1,0);
+        find(ans,board,n,column,ld,rd,0);
 
-        find(0,n,board,ans,column,ldiagonal,rdiagonal);
         return ans;
     }
 };
